a918e443b513b1e2576e3be8f561a2b4c2faf838
Ousmane <kontaoousmane40@gmail.com>
2024-01-09 03:08:38 +0100

    Hey Ousmane :)

--- gluton_algo.py
+++ gluton_algo.py
@@ -1,58 +1,58 @@
 def gluton_coloriage(graph:dict):
     '''
-    Takes in a graph object and return a dict with the right color associated with each vertex
+    Takes in a graph object and return a dict with the right color associated with each sommet
     '''
     color_choices = ["green", "red", "blue", "yellow", "magenta", "cyan", "gray", "black", "white", "purple"]
 
-    couleurs_dict = {}  # {"vertex" : index }
+    couleurs_dict = {}  # {"sommet" : index }
 
-    # color all the childs of a vertex based on its base color
-    def color_child(key:str = None, couleurs_possibles:set = None, start:str = None):
+    # color all the enfants of a sommet based on its base color
+    def color_enfant(key:str = None, couleurs_possibles:set = None, start:str = None):
 
         couleurs_possibles.discard(couleurs_dict[key])
 
         if start and key == start:
-            return
+            return 
 
-        for child in graph[key]:
-            if child in couleurs_dict :
-                child_color = couleurs_dict[child]
+        for enfant in graph[key]:
+            if enfant in couleurs_dict :
+                enfant_color = couleurs_dict[enfant]
                 parent_color = couleurs_dict.get(key, None)
                 
-                #  match with parent color
-                if parent_color is not None and child_color == parent_color :
+                #  match avec la couleur du parent 
+                if parent_color is not None and enfant_color == parent_color :
                     couleurs_possibles.discard(parent_color)
-                    couleurs_dict[child] = min(couleurs_possibles)
-                    couleurs_possibles.discard(couleurs_dict[child])
+                    couleurs_dict[enfant] = min(couleurs_possibles)
+                    couleurs_possibles.discard(couleurs_dict[enfant])
 
-                    color_child(child, couleurs_possibles=couleurs_possibles, start=key)
+                    color_enfant(enfant, couleurs_possibles=couleurs_possibles, start=key)
                 
                 else :
-                    couleurs_possibles.discard(couleurs_dict[child])
+                    couleurs_possibles.discard(couleurs_dict[enfant])
 
             else : 
-                couleurs_dict[child] = min(couleurs_possibles)
-                couleurs_possibles.discard(couleurs_dict[child])
+                couleurs_dict[enfant] = min(couleurs_possibles)
+                couleurs_possibles.discard(couleurs_dict[enfant])
 
 
-    # traverse graph
-    for vertex in graph:
+    # traverse le graphe
+    for sommet in graph:
         couleurs_possibles = set(range(len(color_choices)))  
 
-        if vertex in couleurs_dict :
-            couleurs_possibles.discard(couleurs_dict[vertex])
+        if sommet in couleurs_dict :
+            couleurs_possibles.discard(couleurs_dict[sommet])
         
         else : 
-            couleurs_dict[vertex] = min(couleurs_possibles)
-            couleurs_possibles.discard(couleurs_dict[vertex])
+            couleurs_dict[sommet] = min(couleurs_possibles)
+            couleurs_possibles.discard(couleurs_dict[sommet])
             
-        # traverse all vertices
-        color_child(vertex, couleurs_possibles=couleurs_possibles)
+        # traverse toutes aretes
+        color_enfant(sommet, couleurs_possibles=couleurs_possibles)
 
-        # assign the lowest color
-        if couleurs_possibles and vertex not in couleurs_dict:
+        # assigne la plus petite couleur
+        if couleurs_possibles and sommet not in couleurs_dict:
             couleur_attribuee = min(couleurs_possibles)
-            couleurs_dict[vertex] = couleur_attribuee
-            couleurs_possibles.discard(couleurs_dict[vertex])
+            couleurs_dict[sommet] = couleur_attribuee
+            couleurs_possibles.discard(couleurs_dict[sommet])
 
     return {key : color_choices[value] for key, value in couleurs_dict.items()}
